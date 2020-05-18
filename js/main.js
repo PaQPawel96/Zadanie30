1ac49855fb7aa74442e3b5806131066b4d62136d
PaQPaweł <paq-pawel1996@o2.pl>
2020-05-18 22:06:38 +0200

    Zadanie30

--- js/main.js
+++ js/main.js
@@ -1,1 +1,2 @@
-let paragraf=document.querySelector('#par');
+let tekst=document.getElementById("par").textContent;
+document.getElementById("par").textContent="JavaScript – skryptowy język programowania, stworzony przez firmę Netscape, najczęściej stosowany na stronach internetowych. Twórcą JavaScriptu jest Brendan Eich. W połowie lat 90.";
